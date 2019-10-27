4e941cb45a7d2f782626bc5e052efc167013bfd6
shibin <shibintv@hotmail.com>
2019-10-27 00:10:24 -0500

    image download path

--- webcam/capture/template/select.py
+++ webcam/capture/template/select.py
@@ -12,12 +12,32 @@ from capture.util import util
 
 class Select(APIView):
 
-    def get(self, request):
-        result = []
-        for file in os.listdir(os.environ['CUT_FOLDER']):
-            result.append( {'file_name': file} )
+    def get(self, request,file_name='None'):
         
-        return Response( {'files': result}, 200)
+        if( not file_name):
+            result = []
+            for file in os.listdir(os.environ['CUT_FOLDER']):
+                result.append( {'file_name': '/select/'+file} )
+        
+            return Response( {'files': result}, 200)
+
+        file_name = os.path.join(os.environ['CUT_FOLDER'], file_name)
+        fs = FileSystemStorage()
+        obj = fs.open(file_name)
+        response = Response()
+        response.status_code = 200
+        #response['Content-Disposition'] = 'attachment; filename={}.jpg'.format(file_name)
+        #response.write("hello")
+        content = b''
+        with open(file_name,'rb') as f:
+            for line in f:
+                content += line
+
+        #obj.close()
+        response = Response( base64.b64encode( content ))
+        return response
+
+
     ## pick the file name
     def post(self, request, file_name):
         util.find_similar(file_name)
