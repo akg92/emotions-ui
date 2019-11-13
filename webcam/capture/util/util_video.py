b25b29c5463a48aaa8be01985b38cf9822eeb31d
shibin <shibintv@hotmail.com>
2019-11-13 17:59:29 -0600

    change the frame rate

--- webcam/capture/util/util_video.py
+++ webcam/capture/util/util_video.py
@@ -58,7 +58,7 @@ def get_frames(out_folder, k = 2):
         video_file = os.path.join(video_folder, file)
         video_file_prefix = os.path.splitext(file)[0]
         cap = cv2.VideoCapture(video_file)
-        frame_rate = cap.get(5)/k
+        frame_rate = cap.get(cv2.cv.CV_CAP_PROP_FPS)/k
 
         while(cap.isOpened()):
 
